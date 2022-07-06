f35779c3f8e7817233adae2e2f348c1aa8deb9a9
AuroraTopojani <at28485@seeu.edu.mk>
2022-07-06 10:23:12 +0200

    Updated the portfolio

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -16,20 +16,9 @@
       </ul>
     </section>
     </div>
-
-
 </main>
 </template>
 
-
-<div id="app">
-<nav>
-  <router-link to="/">Home</router-link> |
-  <router-link to="/about">About</router-link>
-
-</nav>
-<router-view/>
-</div>
 <script>
 
 
