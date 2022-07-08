f83ef89c0baf37c6b63c087d6bc3423aff6350b4
AuroraTopojani <at28485@seeu.edu.mk>
2022-07-08 12:40:16 +0200

    Updated the header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="container-fluid bg-header rounded-pill p-2 shadow">
     <div class="row justify-content-center">
-      <div class="col-sm-12 col-lg-12 img-fluid ">
+      <div class="col-sm-2 col-lg-2 img-fluid">
         <img
             src="../assets/images/Picture2.jpg"
             alt="icon"
-            class="ratio ratio-5x3 col-sm-12 col-lg-12 img-fluid"
+            class="ratio ratio-5x3"
         />
       </div>
-      <div class="col-sm-12 col-lg-12  d-flex justify-content-center flex-column py-2">
+      <div class="col-sm-10 col-lg-3 d-flex justify-content-center flex-column">
         <h1>Aurora Topojani</h1>
         <h2>Student of CST</h2>
       </div>
-      <div class="col-sm-12 col-lg-12  d-flex justify-content-center flex-column py-2">
+      <div class="col-lg-3 col-sm-2 d-flex justify-content-center flex-column">
         <div class="m-5">
           <h4 class="d-flex justify-content-end mb-1">Social Links</h4>
           <div class="d-flex justify-content-end mb-2">
